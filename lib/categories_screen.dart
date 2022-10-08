ccbf425171f4c60563284b5c4060122d26dead34
Sajjad Heydari <sajjadheydari1401@gmail.com>
2022-10-08 20:59:02 +0330

    Added Grid View for CategoriesScreen

---
+++ lib/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        'DeliMeal',
+      )),
+      body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((catData) => CategoryItem(
+                    title: catData.title,
+                    color: catData.color,
+                  ))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20)),
+    );
+  }
+}
