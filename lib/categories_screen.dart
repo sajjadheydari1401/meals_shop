e9dada72f2663e6e9882e6c9e977739ae86450fd
Sajjad Heydari <sajjadheydari1401@gmail.com>
2022-10-08 21:35:02 +0330

    add fonts

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -14,6 +14,7 @@ class CategoriesScreen extends StatelessWidget {
         'DeliMeal',
       )),
       body: GridView(
+          padding: EdgeInsets.all(25),
           children: DUMMY_CATEGORIES
               .map((catData) => CategoryItem(
                     title: catData.title,
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'RaleWay',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodySmall: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleLarge: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
       home: CategoriesScreen(),
     );
   }
